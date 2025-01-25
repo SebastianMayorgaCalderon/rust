cf44ef1b6ace8aee8afd2d59e03f35b8a3403c60
Sebastian <sebasmayorga7@gmail.com>
2025-01-24 19:52:18 -0600

    added functions

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
-    //inmutable variable
-   let bunnies: i32; 
-   if true{
-    bunnies = 10;
-   } else {
-    bunnies = 0;
-   }
-    println!("Hello, world! I have {bunnies} bunnies");
+    let text: String=my_function(1, "jhon");
+    let text2: String=my_function2(2, "mario");
+    println!("{}", text);
+    println!("{}", text2);
+}
+
+fn my_function(param1:i32, param2: &str) -> String{
+    format!("hello {} {}", param1, param2)
+}
+fn my_function2(param1:i32, param2: &str) -> String{
+    return format!("hello {} {}", param1, param2);
 }
